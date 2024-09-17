c45d556dcc0e1d756bab46085101f35a26604ab3
Alejo Vilches <alejoevilches@gmail.com>
2024-09-17 18:38:24 -0300

    add decimal fix to prom

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const onSubmit = (event) => {
     rainyDays.push({date, mm});
     alert("Registro exitoso");
     updateProm();
+    showProm()
 }
 
 const updateProm = () => {
@@ -21,5 +22,16 @@ const updateProm = () => {
         total += parseInt(day.mm);
     })
     prom = total / length;
-    document.getElementById("prom_number").innerText = prom;
 }
+
+const showProm = () => {
+    const container = document.querySelector("#prom_number");
+    console.log(container);
+    if (rainyDays.length === 0) {
+        container.innerHTML = "Ingresa un registro para conocer el promedio de lluvias";
+    } else {
+        container.innerHTML = `El promedio de lluvia en los dias cargados es de ${(prom * 100).toFixed(0) / 100}`;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", showProm);
