d487bc58b5f67ee94b61a5b9f45cd3b234c9eeb0
Alejo Vilches <alejoevilches@gmail.com>
2024-09-17 18:25:37 -0300

    add prom

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const rainyDays = [];
 
 const day=new Date()
 
+let prom = 0;
+
 const onSubmit = (event) => {
     event.preventDefault();
     const data=new FormData(event.target);
     const date = data.get("date");
     const mm = data.get("mm");
-    console.log(day.getDate())
     rainyDays.push({date, mm});
     alert("Registro exitoso");
+    updateProm();
+}
+
+const updateProm = () => {
+    const length = rainyDays.length;
+    let total = 0;
+    rainyDays.forEach((day) => {
+        total += parseInt(day.mm);
+    })
+    prom = total / length;
+    document.getElementById("prom_number").innerText = prom;
 }
